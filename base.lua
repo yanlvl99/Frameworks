226989a52a9ea03583148371d26e083e8b947020
Yan <121140658+yanlvl99@users.noreply.github.com>
2024-11-22 18:49:15 -0300

    Create base.lua

---
+++ base.lua
@@ -0,0 +1,3 @@
+Library = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+Window = Library:CreateWindow({ Title = "Yan's Hub", SubTitle = gameName, TabWidth = 112, Size = UDim2.fromOffset(406, 322), Theme = "Darker", MinimizeKey = Enum.KeyCode.LeftAlt})
+Library:ToggleTransparency(false)
